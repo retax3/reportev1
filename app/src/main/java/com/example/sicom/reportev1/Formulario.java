c4986c5ced9875bdc6aaedc212215b5fe4cace8f
retax3 <fretama@alumnos.ubiobio.cl>
2018-07-17 14:57:38 -0400

    first
    validacion de usuario y foto con vista previa

---
+++ app/src/main/java/com/example/sicom/reportev1/Formulario.java
@@ -0,0 +1,51 @@
+package com.example.sicom.reportev1;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class Formulario extends AppCompatActivity {
+static final int requestCamara =1;
+ImageView vistaPrevia;
+Button camara;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+
+        camara = findViewById(R.id.foto);
+        vistaPrevia = findViewById(R.id.viewCamara);
+
+        camara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tomarFoto();
+            }
+        });
+    }
+
+
+
+    private void tomarFoto(){
+        Intent foto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (foto.resolveActivity(getPackageManager())!= null){
+            startActivityForResult(foto,requestCamara);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode==requestCamara && resultCode==RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            vistaPrevia.setImageBitmap(imageBitmap);
+        }
+    }
+}
--- app/src/main/java/com/example/sicom/reportev1/MainActivity.java
+++ app/src/main/java/com/example/sicom/reportev1/MainActivity.java
@@ -5,9 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class  MainActivity extends AppCompatActivity {
 Button btnIngresar;
+EditText nombre,apellidos,rut;
+String strName,strSecondNames,strRUT;
 
 
     @Override
@@ -16,13 +20,52 @@ Button btnIngresar;
         setContentView(R.layout.activity_main);
 
         btnIngresar = findViewById(R.id.bIngresar);
+        nombre = findViewById(R.id.editNombre);
+        apellidos = findViewById(R.id.editApellido);
+        rut = findViewById(R.id.editRUT);
 
         btnIngresar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this,Categorias.class);
-                startActivity(intent);
+                strRUT=rut.getText().toString();
+                strName=nombre.getText().toString();
+                strSecondNames=apellidos.getText().toString();
+
+                if (validarRut(strRUT)==true) {
+                    if (strName != null && strSecondNames != null) {
+                        Intent intent = new Intent(MainActivity.this, Categorias.class);
+                        startActivity(intent);
+                    }
+                    else Toast.makeText(MainActivity.this, "Por favor " +
+                                    "llene todos los campos"
+                            , Toast.LENGTH_SHORT).show();
+                }
+                else Toast.makeText(MainActivity.this, "Su RUT es invalido intente otra vez", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private static boolean validarRut(String rut){
+        boolean validacion = false;
+        try {
+            rut =  rut.toUpperCase();
+            rut = rut.replace(".", "");
+            rut = rut.replace("-", "");
+            int rutAux = Integer.parseInt(rut.substring(0, rut.length() - 1));
+
+            char dv = rut.charAt(rut.length() - 1);
+
+            int m = 0, s = 1;
+            for (; rutAux != 0; rutAux /= 10) {
+                s = (s + rutAux % 10 * (9 - m++ % 6)) % 11;
+            }
+            if (dv == (char) (s != 0 ? s + 47 : 75)) {
+                validacion = true;
+            }
+
+        } catch (java.lang.NumberFormatException e) {
+        } catch (Exception e) {
+        }
+        return validacion;
     }
 }
